8cb97741ef9dfd8b5bea0ed2499a582edaa7bf27
fuy3 <Yu.Fu3@emc.com>
2018-04-22 21:45:34 +0800

    Delete some test code.

--- testmodel.py
+++
@@ -1,6 +0,0 @@
-import settings
-from Domains.models import *
-
-test_str = '{"domainName":"biubiu.org","locked":true,"autorenewEnabled":true,"expireDate":"2020-12-14T05:06:50Z","createDate":"2012-12-14T05:06:50Z"}'
-
-d = Domain.from_json(test_str)
